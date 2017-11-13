8ae9181446d13c0e95737c02f0495c85309a1eb1
jumarbach <32646659+jumarbach@users.noreply.github.com>
2017-11-13 18:28:19 +0100

    Add files via upload

--- code/step/krill_step.m
+++ code/step/krill_step.m
@@ -1,22 +1,22 @@
 function [ A ] = krill_step( K )
 %boids step - addition of vectors towards neighbours
-%   K g^has to be of size 2xn
+%   K g^has to be of size nx2
 
     sigma = 2; %neighbours range
     mov = zeros(size(K));
     
-    for i=1:size(K,2)
-        for j=i+1:size(K,2)
-            v = [K(1,i) - K(1,j) , K(2,i) - K(2,j)];
+    for i=1:size(K,1)
+        for j=i+1:size(K,1)
+            v = [K(i,1) - K(j,1) , K(i,2) - K(j,2)];
             %v is the vector between krill i and neighbor j;
             
             if norm(v) < sigma
                 %half of vector v is added to the movement of both krills i
                 %and j
-                mov(1,i) = mov(1,i) - 0.5*v(1);
-                mov(2,i) = mov(2,i) - 0.5*v(2);
-                mov(1,j) = mov(1,j) + 0.5*v(1);
-                mov(2,j) = mov(2,j) + 0.5*v(2);
+                mov(i,1) = mov(i,1) - 0.5*v(1);
+                mov(i,2) = mov(i,2) - 0.5*v(2);
+                mov(j,1) = mov(j,1) + 0.5*v(1);
+                mov(j,2) = mov(j,2) + 0.5*v(2);
             end
         end
     end
--- code/step/krill_step_midp.m
+++ code/step/krill_step_midp.m
@@ -2,33 +2,34 @@ function [ A ] = krill_step_midp (K)
 %boids step, goes to average position of neighbors
 %   average position is calculated by the addition of de vectors Oi for all
 %   neighbours i and dividing the result by i.
-%   K has to be of size 2xn
+%   K has to be of size nx2
 
 sigma = 2; %neighbours range
 A = zeros(size(K));
 
-for i=1:size(K,2)
+for i=1:size(K,1)
     x = 0;
     y = 0;
     count = 0;
     
-    for j=1:size(K,2)
-        v = [K(1,i) - K(1,j) , K(2,i) - K(2,j)];
+    for j=1:size(2,K)
+        v = [K(i,1) - K(j,1) , K(i,2) - K(j,2)];
         %vector between i and j, used to calculate norm
         if (i ~= j) && (norm(v) < sigma)%test if j neighbour of i and i != j
-            x = x + K(1,j);
-            y = y + K(2,j);
+            x = x + K(j,1);
+            y = y + K(j,2);
             count = count + 1;
         end
     end
     %doesn't move if no neighbours
     if count == 0
         count = 1;
-        x = K(1,i);
-        y = K(2,i);
+        x = K(i,1);
+        y = K(i,2);
     end
-    A(1,i) = x / count;
-    A(2,i) = y / count;
+    %write new positions
+    A(i,1) = x / count;
+    A(i,2) = y / count;
 end
 
 end
